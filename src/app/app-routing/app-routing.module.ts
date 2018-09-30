748e58c00c4cc96028419436a27ac95ba5e2158d
Oriol Rabasseda <orabasal15@alumnes.ub.edu>
2018-09-30 14:44:22 +0200

    Routing fixed and node package added

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  //{ path: '', loadChildren: './main-page/main-page.module#MainPageModule' },
-  //{ path: 'feed', loadChildren: './home-page/home-page#HomePageModule'}
+  { path: '', loadChildren: '../main-page/main-page.module#MainPageModule' },
+  { path: 'feed', loadChildren: '../home-page/home-page.module#HomePageModule'}
 ];
 
 @NgModule({
