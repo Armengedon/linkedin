5ae8124ec8cb8883799f8874534bd18816e4d22a
Oriol Rabasseda <orabasal15@alumnes.ub.edu>
2018-09-30 12:31:14 +0200

    Continuing uploading necessary files

---
+++ src/app/app-routing/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  //{ path: '', loadChildren: './main-page/main-page.module#MainPageModule' },
+  //{ path: 'feed', loadChildren: './home-page/home-page#HomePageModule'}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
