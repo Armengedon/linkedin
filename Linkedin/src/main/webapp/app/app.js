33a915554624e127abf24a9784ab050765f09112
Martín Pérez <mapeya94@gmail.com>
2018-10-17 15:37:38 +0200

    Omplint app.js

--- Linkedin/src/main/webapp/app/app.js
+++ Linkedin/src/main/webapp/app/app.js
@@ -1,1 +1,27 @@
-var a = angular.module('crudaApp',['ngResource']);
+var app = angular.module('Linkedin',['ui.router', 'nStorage', 'ngResource']);
+
+app.constant('urls', {
+	BASE: 'http://localhost:8080',
+	USER_DEVICE_API: 'http://localhost:8080/api/user'
+});
+
+app.config(['$stateProvider', '$urlRouterProvider',
+	function($stateProvider, $urlRouterProvider) {
+	 
+    $stateProvider
+        .state('home', {
+            url: '/',
+            templateUrl: 'partials/list',
+            controller:'user.controller',
+            controllerAs:'ctrl',
+            resolve: {
+                users: function ($q, UserService) {
+                    console.log('Load all users');
+                    var deferred = $q.defer();
+                    UserService.loadAllUsers().then(deferred.resolve, deferred.resolve);
+                    return deferred.promise;
+                }
+            }
+        });
+    $urlRouterProvider.otherwise('/');
+	}]);
