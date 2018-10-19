29f0359c3e6adca01ab74be0478fe965638a48dd
Oriol Rabasseda <orabasal15@alumnes.ub.edu>
2018-10-20 00:44:12 +0200

    main page adapted to new navigation bar and viceversa

--- Linkedin/src/main/webapp/resources/js/navbar.js
+++ Linkedin/src/main/webapp/resources/js/navbar.js
@@ -1,53 +1,39 @@
-var active = "home";
-var getTab = function() {
-    return active;
+var home = function() {
+    document.getElementById("home_image").innerHTML = "<img src='/resources/images/home_icon_active.png'>";
+    document.getElementById("network_image").innerHTML = "<img src='/resources/images/network_icon_inactive.png'>";
+    document.getElementById("jobs_image").innerHTML = "<img src='/resources/images/jobs_icon_inactive.png'>";
+    document.getElementById("messages_image").innerHTML = "<img src='/resources/images/messages_icon_inactive.png'>";
+    document.getElementById("spam_image").innerHTML = "<img src='/resources/images/spam_icon_inactive.png'>";
 };
-var setTab = function(name) {
-    this.active = name;
+
+var network = function() {
+    document.getElementById("home_image").innerHTML = "<img src='/resources/images/home_icon_inactive.png'>";
+    document.getElementById("network_image").innerHTML = "<img src='/resources/images/network_icon_active.png'>";
+    document.getElementById("jobs_image").innerHTML = "<img src='/resources/images/jobs_icon_inactive.png'>";
+    document.getElementById("messages_image").innerHTML = "<img src='/resources/images/messages_icon_inactive.png'>";
+    document.getElementById("spam_image").innerHTML = "<img src='/resources/images/spam_icon_inactive.png'>";
 };
-var initialize = function() {
-    var tab = getTab();
-    
-    if (tab == "home") {
-        document.getElementById("home_image").innerHTML = "<img src='/resources/images/home_icon_active.png'>";
-        document.getElementById("network_image").innerHTML = "<img src='/resources/images/network_icon_inactive.png'>";
-        document.getElementById("jobs_image").innerHTML = "<img src='/resources/images/jobs_icon_inactive.png'>";
-        document.getElementById("messages_image").innerHTML = "<img src='/resources/images/messages_icon_inactive.png'>";
-        document.getElementById("spam_image").innerHTML = "<img src='/resources/images/spam_icon_inactive.png'>";
-    }
-    else if (tab == "network") {
-        document.getElementById("home_image").innerHTML = "<img src='/resources/images/home_icon_inactive.png'>";
-        document.getElementById("network_image").innerHTML = "<img src='/resources/images/network_icon_active.png'>";
-        document.getElementById("jobs_image").innerHTML = "<img src='/resources/images/jobs_icon_active.png'>";
-        document.getElementById("messages_image").innerHTML = "<img src='/resources/images/messages_icon_active.png'>";
-        document.getElementById("spam_image").innerHTML = "<img src='/resources/images/spam_icon_active.png'>";
-    }
-    else if (tab == "jobs") {
-        document.getElementById("home_image").innerHTML = "<img src='/resources/images/home_icon_inactive.png'>";
-        document.getElementById("network_image").innerHTML = "<img src='/resources/images/network_icon_inactive.png'>";
-        document.getElementById("jobs_image").innerHTML = "<img src='/resources/images/jobs_icon_active.png'>";
-        document.getElementById("messages_image").innerHTML = "<img src='/resources/images/messages_icon_inactive.png'>";
-        document.getElementById("spam_image").innerHTML = "<img src='/resources/images/spam_icon_inactive.png'>";
-    }
-    else if (tab == "messages") {
-        document.getElementById("home_image").innerHTML = "<img src='/resources/images/home_icon_inactive.png'>";
-        document.getElementById("network_image").innerHTML = "<img src='/resources/images/network_icon_inactive.png'>";
-        document.getElementById("jobs_image").innerHTML = "<img src='/resources/images/jobs_icon_inactive.png'>";
-        document.getElementById("messages_image").innerHTML = "<img src='/resources/images/messages_icon_active.png'>";
-        document.getElementById("spam_image").innerHTML = "<img src='/resources/images/spam_icon_inactive.png'>";
-    }
-    else if (tab == "spam") {
-        document.getElementById("home_image").innerHTML = "<img src='/resources/images/home_icon_inactive.png'>";
-        document.getElementById("network_image").innerHTML = "<img src='/resources/images/network_icon_inactive.png'>";
-        document.getElementById("jobs_image").innerHTML = "<img src='/resources/images/jobs_icon_inactive.png'>";
-        document.getElementById("messages_image").innerHTML = "<img src='/resources/images/messages_icon_inactive.png'>";
-        document.getElementById("spam_image").innerHTML = "<img src='/resources/images/spam_icon_active.png'>";
-    }
-    else {
-        document.getElementById("home_image").innerHTML = "<img src='/resources/images/home_icon_inactive.png'>";
-        document.getElementById("network_image").innerHTML = "<img src='/resources/images/network_icon_inactive.png'>";
-        document.getElementById("jobs_image").innerHTML = "<img src='/resources/images/jobs_icon_inactive.png'>";
-        document.getElementById("messages_image").innerHTML = "<img src='/resources/images/messages_icon_inactive.png'>";
-        document.getElementById("spam_image").innerHTML = "<img src='/resources/images/spam_icon_inactive.png'>";
-    }
+
+var jobs = function() {
+    document.getElementById("home_image").innerHTML = "<img src='/resources/images/home_icon_inactive.png'>";
+    document.getElementById("network_image").innerHTML = "<img src='/resources/images/network_icon_inactive.png'>";
+    document.getElementById("jobs_image").innerHTML = "<img src='/resources/images/jobs_icon_active.png'>";
+    document.getElementById("messages_image").innerHTML = "<img src='/resources/images/messages_icon_inactive.png'>";
+    document.getElementById("spam_image").innerHTML = "<img src='/resources/images/spam_icon_inactive.png'>";
+};
+
+var messages = function() {
+    document.getElementById("home_image").innerHTML = "<img src='/resources/images/home_icon_inactive.png'>";
+    document.getElementById("network_image").innerHTML = "<img src='/resources/images/network_icon_inactive.png'>";
+    document.getElementById("jobs_image").innerHTML = "<img src='/resources/images/jobs_icon_inactive.png'>";
+    document.getElementById("messages_image").innerHTML = "<img src='/resources/images/messages_icon_active.png'>";
+    document.getElementById("spam_image").innerHTML = "<img src='/resources/images/spam_icon_inactive.png'>";
+};
+
+var spam = function() {
+    document.getElementById("home_image").innerHTML = "<img src='/resources/images/home_icon_inactive.png'>";
+    document.getElementById("network_image").innerHTML = "<img src='/resources/images/network_icon_inactive.png'>";
+    document.getElementById("jobs_image").innerHTML = "<img src='/resources/images/jobs_icon_inactive.png'>";
+    document.getElementById("messages_image").innerHTML = "<img src='/resources/images/messages_icon_inactive.png'>";
+    document.getElementById("spam_image").innerHTML = "<img src='/resources/images/spam_icon_active.png'>";
 };
