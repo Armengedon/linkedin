0de76a377e3052b37f1823f681a81aee97a6de4d
Joan <joan180@live.com>
2018-12-03 21:14:58 +0100

    junit4 and EclEmma plugin installation

---
+++ src/test/java/com/ub/tests/UserControllerTest.java
@@ -0,0 +1,84 @@
+package com.ub.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UserControllerTest {
+
+	@Test
+	public void testViewRegister() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCreateUser() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdateUserAppUserInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPerformlogin() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRetrieveAllUsers() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRetrieveUser() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDeleteUser() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdateUserAppUserLong() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddStudies() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddJobExperience() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddPersonalInfo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddPublication() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdatePersonalInfo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdateStudies() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdateJobExperience() {
+		fail("Not yet implemented");
+	}
+
+}
